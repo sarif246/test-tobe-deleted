402a626261e0e9ce95ab3fa3aef48668120444ef
sarif246 <57228832+sarif246@users.noreply.github.com>
2023-08-04 18:18:36 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,25 @@
+resource "google_compute_instance" "default" {
+  name         = "test123"
+  machine_type = "e2-medium"
+  zone         = "us-central1-a"
+
+  tags = ["foo", "bar"]
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-11"
+      labels = {
+        my_label = "value"
+      }
+    }
+  }
+
+
+  network_interface {
+    network = "default"
+
+    access_config {
+      // Ephemeral public IP
+    }
+  }
+}
